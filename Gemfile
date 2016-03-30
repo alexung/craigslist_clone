0a47f6ef0a4953096e9397710aed359e2aa3fca6
Alex Ung <alexung.ucsb@gmail.com>
2016-03-29 22:21:34 -0700

    add gem forgery

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
 ruby '2.3.0'
+
+#for lorem ipsum
+gem 'forgery'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use postgresql as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(title: 'Ruby');
+Article.create(category_id: 1, title: 'Ruby on Rails', description: "It's a neat framework.", )
