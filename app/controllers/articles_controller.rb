c3115080ebba77c6dbd24a2e0cbfd6468ffdbc14
Alex Ung <alexung.ucsb@gmail.com>
2016-03-30 13:17:18 -0700

    creating post functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,23 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  # renders the page to input all your fields
   def new
+    @article = Article.new
   end
+
+  # triggered on the 'post' button on new page
+  def create
+    @article = Article.new(article_params)
+    @article.category = Category.find(params[:category_id])
+    @article.save
+      redirect_to category_path(@article.category)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit([:title, :description, :address, :price])
+  end
+
 end
