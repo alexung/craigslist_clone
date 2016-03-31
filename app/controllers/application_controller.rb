56347d142f9134918ca06b98ee95b3c9237e5c8a
Alex Ung <alexung.ucsb@gmail.com>
2016-03-30 22:00:45 -0700

    api works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :get_twitter_api
+  def get_twitter_api
+    @client = Twitter::Streaming::Client.new do |config|
+      config.consumer_key        = "YOUR_CONSUMER_KEY"
+      config.consumer_secret     = "YOUR_CONSUMER_SECRET"
+      config.access_token        = "YOUR_ACCESS_TOKEN"
+      config.access_token_secret = "YOUR_ACCESS_SECRET"
+    end
+  end
 end
